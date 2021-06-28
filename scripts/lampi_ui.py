99c9ad65860a6a923c91ea39b93a37cb164b0e26
Thiago Silva <thiago.sf.silva@stir.ac.uk>
2021-06-28 08:45:24 +0100

    UI elements and keys fully defined, ready for including events

--- scripts/lampi_ui.py
+++ scripts/lampi_ui.py
@@ -2,32 +2,32 @@ from types import TracebackType
 import PySimpleGUI as sg
 import os.path
 
-sg.theme('Black')   # Add a touch of color
+sg.theme('System Default 1')   # Add a touch of color
 
 # Elements
 
 btn_pinum = sg.OptionMenu(
-    [1,2,3,4,5,6,7,8,9,10],
+    [1,2,3,4,5,6,7,8,9,10], key='-PINUM-'
 )
 
 btn_res = sg.OptionMenu(
-    ['640×480', '800×600', '960×720', '1024×768', '1280×960', '1400×1050', '1440×1080'],
+    [(640,480), (800,600), (960,720), (1024,768), (1280,960), (1400,1050), (1440,1080)],  key='-RES-'
 )
 
 btn_fps = sg.OptionMenu(
-    [10,15,30,60],
+    [10,15,30,60],  key='-FPS-'
 )
 
 btn_cliplen = sg.OptionMenu(
-    [1,3,10,30,60,90,120,300,600],
+    [1,3,10,30,60,90,120,300,600], key='-CLEN-'
 )
 
 btn_strtime = sg.OptionMenu(
-    [1,2,3,4,5,6,7,8,9,10],
+    [1,2,3,4,5,6,7,8,9,10],  key='-STRT-'
 )
 
 btn_endtime = sg.OptionMenu(
-    [1,3,10,30,60,90,120,300,600],
+    [1,3,10,30,60,90,120,300,600],  key='-ENDT-'
 )
 
 # Setup column layout
@@ -38,13 +38,14 @@ setup_column = [
     [sg.Text('Duration (s)'), btn_cliplen],
     [sg.Text('Start/End Time'), btn_strtime, btn_endtime],
     [sg.Text('Save path:')],
-    [sg.In(size=(25,1), enable_events=True, key='-FOLDER-'), sg.FolderBrowse()]
+    [sg.In(size=(25,1), enable_events=True, key='-FOLDER-'), sg.FolderBrowse()]    
 ]
 
 #  Setup run layout         
 
 run_column = [
-    [sg.Button('Start'), sg.Button('Stop')]
+    [sg.Button('Start', size=(25,2), enable_events=True,  key='-START-')],
+    [sg.Multiline('Select settings and hit "Start"', size=(27,5), key='-TEXTBOX-')]
 ] 
 
 
@@ -62,6 +63,6 @@ while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
         break
-    print('You entered ', values[0])
+    window['-TEXTBOX-'].print('You entered ', values['-CLEN-'])
 
 window.close()
