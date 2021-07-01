8e6ee5039f5c30fea83bc9089336d7bea49e3c39
Thiago Silva <thiago.sf.silva@stir.ac.uk>
2021-07-01 11:23:24 +0100

    trying to resize comboboxes - didnae work

--- scripts/lampi_tkinter_ui.py
+++ scripts/lampi_tkinter_ui.py
@@ -9,92 +9,105 @@ import pickle
 ### UI Functions
 # Function to save the recording parameters
 def save_params(params):
-    pickle.dump(params, open("params.p", "wb" ))
+    pickle.dump(params, open("params.p", "wb"))
+
 
 ### Main UI
 
-# root and framne UI definitions
+# root window and framne UI definitions
 root = Tk()
 root.title("Lampi Ui Tk")
+root.geometry("480x320+5+5")
 
-mainframe = ttk.Frame(root, padding="3 3 12 12")
-mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
-root.columnconfigure(0, weight=1)
-root.rowconfigure(0, weight=1)
+# mainframe = ttk.Frame(root, padding="3 3 3 3")
+# mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+root.columnconfigure(1, weight=1)
+root.columnconfigure(2, weight=1)
+root.columnconfigure(3, weight=1)
+root.columnconfigure(4, weight=1)
+root.columnconfigure(5, weight=4)
 
 ### Define paramter widgets
 
 # Select pi number from dropdown list
 piNum = IntVar()
-piNumCombobox = ttk.Combobox(mainframe, textvariable=piNum)
-piNumCombobox['values'] = (1,2,3,4,5,6,7,8,9,10)
-piNumCombobox.state(['readonly'])
+piNumCombobox = ttk.Combobox(root, textvariable=piNum)
+piNumCombobox["values"] = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+piNumCombobox.state(["readonly"])
 
 # Select video resolution
 vidRes = IntVar()
-vidResCombobox = ttk.Combobox(mainframe, textvariable=vidRes)
-vidResCombobox['values'] = ((640,480), (800,600), (960,720), (1024,768), (1280,960), (1400,1050), (1440,1080))
-vidResCombobox.state(['readonly'])
+vidResCombobox = ttk.Combobox(root, textvariable=vidRes)
+vidResCombobox["values"] = (
+    (640, 480),
+    (800, 600),
+    (960, 720),
+    (1024, 768),
+    (1280, 960),
+    (1400, 1050),
+    (1440, 1080),
+)
+vidResCombobox.state(["readonly"])
 
-# Select frame rate 
+# Select frame rate
 vidFPS = IntVar()
-vidFPSCombobox = ttk.Combobox(mainframe, textvariable=vidFPS)
-vidFPSCombobox['values'] = (10,15,30,60)
-vidFPSCombobox.state(['readonly'])
+vidFPSCombobox = ttk.Combobox(root, width=2, textvariable=vidFPS)
+vidFPSCombobox["values"] = (10, 15, 30, 60)
+vidFPSCombobox.state(["readonly"])
 
-# Select video duration 
+# Select video duration
 vidDur = IntVar()
-vidDurCombobox = ttk.Combobox(mainframe, textvariable=vidDur)
-vidDurCombobox['values'] = (1,3,10,30,60,120,300,600)
-vidDurCombobox.state(['readonly'])
+vidDurCombobox = ttk.Combobox(root, width=3, textvariable=vidDur)
+vidDurCombobox["values"] = (1, 3, 10, 30, 60, 120, 300, 600)
+vidDurCombobox.state(["readonly"])
 
-# Select recording start time  
+# Select recording start time
 vidStrt = IntVar()
-vidStrtCombobox = ttk.Combobox(mainframe, textvariable=vidStrt)
-vidStrtCombobox['values'] = (1,3,10,30,60,120,300)
-vidStrtCombobox.state(['readonly'])
+vidStrtCombobox = ttk.Combobox(root, width=3, textvariable=vidStrt)
+vidStrtCombobox["values"] = (1, 3, 10, 30, 60, 120, 300)
+vidStrtCombobox.state(["readonly"])
 
-# Select recording end time  
+# Select recording end time
 vidEnd = IntVar()
-vidEndCombobox = ttk.Combobox(mainframe, textvariable=vidEnd)
-vidEndCombobox['values'] = (1,3,10,30,60,120,300)
-vidEndCombobox.state(['readonly'])
+vidEndCombobox = ttk.Combobox(root, width=3, textvariable=vidEnd)
+vidEndCombobox["values"] = (1, 3, 10, 30, 60, 120, 300)
+vidEndCombobox.state(["readonly"])
 
-# Select out of hours timelapse timing   
+# Select out of hours timelapse timing
 timeLapse = IntVar()
-timeLapseCombobox = ttk.Combobox(mainframe, textvariable=timeLapse)
-timeLapseCombobox['values'] = (1,3,10,30,60,120,300)
-timeLapseCombobox.state(['readonly'])
+timeLapseCombobox = ttk.Combobox(root, width=3, textvariable=timeLapse)
+timeLapseCombobox["values"] = (1, 3, 10, 30, 60, 120, 300)
+timeLapseCombobox.state(["readonly"])
 
 # Option to restart with saved pameters
 reStart = BooleanVar()
-reStartCheckbutton = ttk.Checkbutton(mainframe, text='Autostart recording on boot', variable=reStart)
-	    
+reStartCheckbutton = ttk.Checkbutton(
+    root, text="Autostart recording on boot", variable=reStart
+)
+
 
 ### UI layout
 
 # Left columns (0,1)
-ttk.Label(mainframe, text="Pi Number ").grid(column=0, row=1, sticky=W)
-piNumCombobox.grid(column=1, row=1, sticky=(W, E))
+ttk.Label(root, text="Pi Number ").grid(column=1, row=1, sticky=W)
+piNumCombobox.grid(column=2, row=1, sticky=(W, E))
 
-ttk.Label(mainframe, text="Video resolution ").grid(column=0, row=2, sticky=W)
-vidResCombobox.grid(column=1, row=2, sticky=(W, E))
+ttk.Label(root, text="Video resolution ").grid(column=1, row=2, sticky=W)
+vidResCombobox.grid(column=2, row=2, sticky=(W, E))
 
-ttk.Label(mainframe, text="Video frame rate ").grid(column=0, row=3, sticky=W)
-vidFPSCombobox.grid(column=1, row=3, sticky=(W, E))
+ttk.Label(root, text="Video frame rate ").grid(column=1, row=3, sticky=W)
+vidFPSCombobox.grid(column=2, row=3, sticky=(W, E))
+ttk.Label(root, text=" Clip duration ").grid(column=3, row=3, sticky=W)
+vidDurCombobox.grid(column=4, row=3, sticky=(W, E))
 
-ttk.Label(mainframe, text=" Clip duration ").grid(column=2, row=3, sticky=W)
-vidDurCombobox.grid(column=3, row=3, sticky=(W, E))
+ttk.Label(root, text="Start recording at ").grid(column=1, row=4, sticky=W)
+vidStrtCombobox.grid(column=2, row=4, sticky=(W, E))
+ttk.Label(root, text=" End at ").grid(column=3, row=4, sticky=W)
+vidEndCombobox.grid(column=4, row=4, sticky=(W, E))
 
-ttk.Label(mainframe, text="Start recording at ").grid(column=0, row=4, sticky=W)
-vidStrtCombobox.grid(column=1, row=4, sticky=(W, E))
+ttk.Label(root, text=" Out of hours timelapse rate ").grid(column=1, row=5, sticky=W)
+timeLapseCombobox.grid(column=2, row=5, sticky=(W, E))
 
-ttk.Label(mainframe, text=" End at ").grid(column=2, row=4, sticky=W)
-vidEndCombobox.grid(column=3, row=4, sticky=(W, E))
-
-ttk.Label(mainframe, text=" Out of hours timelapse rate ").grid(column=0, row=5, sticky=W)
-timeLapseCombobox.grid(column=1, row=5, sticky=(W, E))
-
-reStartCheckbutton.grid(column=0, row=6)
+reStartCheckbutton.grid(column=1, row=6)
 
 root.mainloop()
--- scripts/lampi_ui.py
+++ scripts/lampi_ui.py
@@ -1,108 +1,110 @@
 # from types import TracebackType
-import PySimpleGUI as sg # to create and run the UI
-import os.path # to handle file paths
-import pickle # to save parameters
+import PySimpleGUI as sg  # to create and run the UI
+import os.path  # to handle file paths
+import pickle  # to save parameters
 
 ##### Define / Import functions ######
 
-def save_params(params):
-    pickle.dump(params, open("params.p", "wb" ))
 
-###### UI APP ########## 
+def save_params(params):
+    pickle.dump(params, open("params.p", "wb"))
+
+
+###### UI APP ##########
 
 ## UI Elements ##
 
 # Buttons #
 
 # Select pi number
-btn_pinum = sg.OptionMenu(
-    [1,2,3,4,5,6,7,8,9,10], key='-PINUM-'
-)
+btn_pinum = sg.OptionMenu([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], key="-PINUM-")
 
 # Select video resolution
 btn_res = sg.OptionMenu(
-    [(640,480), (800,600), (960,720), (1024,768), (1280,960), (1400,1050), (1440,1080)],  key='-RES-'
+    [
+        (640, 480),
+        (800, 600),
+        (960, 720),
+        (1024, 768),
+        (1280, 960),
+        (1400, 1050),
+        (1440, 1080),
+    ],
+    key="-RES-",
 )
 
-# select video framerate 
-btn_fps = sg.OptionMenu(
-    [10,15,30,60],  key='-FPS-'
-)
+# select video framerate
+btn_fps = sg.OptionMenu([10, 15, 30, 60], key="-FPS-")
 # select clip length
-btn_cliplen = sg.OptionMenu(
-    [1,3,10,30,60,90,120,300,600], key='-CLEN-'
-)
+btn_cliplen = sg.OptionMenu([1, 3, 10, 30, 60, 90, 120, 300, 600], key="-CLEN-")
 
 # select recording start time
-btn_strtime = sg.OptionMenu(
-    [1,2,3,4,5,6,7,8,9,10],  key='-STRT-'
-)
+btn_strtime = sg.OptionMenu([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], key="-STRT-")
 
 # select recording end time
-btn_endtime = sg.OptionMenu(
-    [1,3,10,30,60,90,120,300,600],  key='-ENDT-'
-)
+btn_endtime = sg.OptionMenu([1, 3, 10, 30, 60, 90, 120, 300, 600], key="-ENDT-")
 
 # save parameters
-btn_save = sg.Button('Save', enable_events=True, key = '-SAVE-')
+btn_save = sg.Button("Save", enable_events=True, key="-SAVE-")
 
 # start recording
-btn_start = sg.Button('Start', size=(25,1), enable_events=True,  key='-START-')
+btn_start = sg.Button("Start", size=(25, 1), enable_events=True, key="-START-")
 
 # Checkboxes #
 
 chbx_daytime = sg.Checkbox(
-    'Enable 2sec timelapse outside recording hours?', default=False, key = '-DTLPS-'
-    )
+    "Enable 2sec timelapse outside recording hours?", default=False, key="-DTLPS-"
+)
 
 # Multiline text box #
 
 out_box = sg.Multiline(
     """1) Select settings
     2) Save settings
-    3) Click 'Start' """
-    , size=(27,8), key='-TEXTBOX-') 
+    3) Click 'Start' """,
+    size=(27, 8),
+    key="-TEXTBOX-",
+)
 
 
 ## UI Layout and appearabnce ##
 
-sg.theme('System Default 1')   # Set colour theme
+sg.theme("System Default 1")  # Set colour theme
 
 # Set left (parameter input ) column layout
-setup_column = [  
-    [sg.Text('Pi Number'), btn_pinum], 
-    [sg.Text('Video Resolution'), btn_res],
-    [sg.Text('Video frame rate'), btn_fps, sg.Text('Duration (s)'), btn_cliplen],
-    [sg.Text('Start recording at: '), btn_strtime, sg.Text(' End at: '), btn_endtime],
-    [sg.Text('Save path:'), sg.In(size=(25,1), key='-FOLDER-'), sg.FolderBrowse()],
-    [chbx_daytime, btn_save]
+setup_column = [
+    [sg.Text("Pi Number"), btn_pinum],
+    [sg.Text("Video Resolution"), btn_res],
+    [sg.Text("Video frame rate"), btn_fps, sg.Text("Duration (s)"), btn_cliplen],
+    [sg.Text("Start recording at: "), btn_strtime, sg.Text(" End at: "), btn_endtime],
+    [sg.Text("Save path:"), sg.In(size=(25, 1), key="-FOLDER-"), sg.FolderBrowse()],
+    [chbx_daytime, btn_save],
 ]
 
-#  Set right (run app) layout         
-run_column = [
-    [btn_start],
-    [out_box]
-] 
+#  Set right (run app) layout
+run_column = [[btn_start], [out_box]]
 
 # Set 2-column window layout
 layout = [
-    [sg.Column(setup_column), # left column
-    sg.VSeparator(), # vertical separator (line)
-    sg.Column(run_column)] # right column
+    [
+        sg.Column(setup_column),  # left column
+        sg.VSeparator(),  # vertical separator (line)
+        sg.Column(run_column),
+    ]  # right column
 ]
 
 ### App execution ###
 
-# Create the Window 
-window = sg.Window('LamPi UI', layout)
+# Create the Window
+window = sg.Window("LamPi UI", layout)
 
-# Event Loop to process events (get values and run functions) 
+# Event Loop to process events (get values and run functions)
 while True:
     event, values = window.read()
     if event == "-SAVE-":
         save_params(values)
-        window['-TEXTBOX-'].print('\nParameters saved:\n', values)
-    if event == sg.WIN_CLOSED: # ends program if user closes window
+        window["-TEXTBOX-"].print("\nParameters saved:\n", values)
+    if event == sg.WIN_CLOSED:  # ends program if user closes window
         break
- 
+
 window.close()
