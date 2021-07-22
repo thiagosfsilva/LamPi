b6cab987feca52207596a323460ba77754cc515f
Thiago Silva <thiago.sf.silva@stir.ac.uk>
2021-07-22 17:46:57 +0100

    first version of new timerange implementation

--- scripts/dev/daterange.py
+++ scripts/dev/daterange.py
@@ -1,18 +1,40 @@
 from datetime import datetime, time, timedelta
 
-now = datetime.combine(datetime.now().date()+timedelta(days=0),time(8,0))
+now = datetime.now()
 
-params = {"-START-": time(5,0), "-END-": time(12,0)}
+start = datetime.combine(now.date(), time(18, 0))
+end1 = timedelta(hours=5)
+end2 = timedelta(hours=12)
 
-def time_in_range(start, end, x):
-    today = datetime.now().date()
-    if start < end:
-        strt = datetime.combine(today,start)
-        endt = datetime.combine(today,end)
-    elif end < start:
-        strt = datetime.combine(today,start)
-        endt = datetime.combine(today+timedelta(days=1),end)
-    result = x >= strt and x <= endt
-    return result
+go1 = datetime.combine(now.date(), time(15, 0))
+go2 = datetime.combine(now.date(), time(20, 0))
+go3 = datetime.combine(now.date(), time(1, 0)) + timedelta(days=1)
+go4 = datetime.combine(now.date(), time(8, 0)) + timedelta(days=1)
 
-print(time_in_range(params["-START-"],params["-END-"], now))
+
+def time_in_range(go, end):
+    global start
+    now = go
+    stop = start + end
+    if now < start:
+        print(f"{now} is before {start}, so I'm waiting")
+    elif now >= start and now <= stop:
+        print(f"{now} is after {start} and before {stop}, so I'm recording")
+    else:
+        start = start + timedelta(days=1)
+        print(
+            f"{now} is after {start} and {stop}, so I'm setting the next start to {start} and waiting"
+        )
+
+
+time_in_range(go1, end1)
+time_in_range(go2, end1)
+time_in_range(go3, end1)
+time_in_range(go4, end1)
+
+start = datetime.combine(now.date(), time(18, 0))
+
+time_in_range(go1, end2)
+time_in_range(go2, end2)
+time_in_range(go3, end2)
+time_in_range(go4, end2)
--- scripts/lampi_run_terminal.py
+++ scripts/lampi_run_terminal.py
@@ -1,19 +1,28 @@
 #!/usr/bin/env python3
 from gpiozero.internal_devices import LoadAverage
 import picamera, pickle, os, time as tm
-from datetime import datetime, time
+from datetime import datetime, time, timedelta
 from gpiozero import CPUTemperature, DiskUsage, LoadAverage
 from shutil import disk_usage
 
 # Function to test if current time is in range
-def time_in_range(start, end):
-    now = datetime.now().time()
-    preMidnight = time(23, 59, 59)
-    midnight = time(0, 0, 0)
-    if (now >= start and now <= preMidnight) or (now >= midnight and now <= end):
-        return True
+def time_in_range(end):
+    global start
+    global recstatus
+    now = datetime.now()
+    stop = start + end
+    if now < start:
+        print(f"{now} is before {start}, so I'm waiting")
+        recstatus = "wait"
+    elif now >= start and now <= stop:
+        print(f"{now} is after {start} and before {stop}, so I'm recording")
+        recstatus = "rec"
     else:
-        return False
+        start = start + timedelta(days=1)
+        print(
+            f"{now} is after {start} and {stop}, so I'm syncing and then waiting until {start}"
+        )
+        recstatus = "sync"
 
 
 # Function to everything
@@ -41,8 +50,9 @@ camera.resolution = eval(params["-RES-"])  # set video resolution
 camera.framerate = int(params.get("-FPS-"))  # set video framerate
 clipDuration = int(params.get("-CLDUR-"))  # set clip duration in seconds
 piNum = params.get("-PINUM-")  # set raspberry pi identifie
-strtm = datetime.strptime(params["-STRT-"], "%H:%M:%S").time()
-endtm = datetime.strptime(params["-ENDT-"], "%H:%M:%S").time()
+start = datetime.combine(datetime.now(), time(params["STARTHR"], params["STARTMIN"]))
+end = timedelta(hours=params["-RECLEN-"])
+recstatus = "wait"
 
 # Start recording logfile
 # logName = f"/home/pi/LamPi/sync/logs/flopi{piNum}_rec_log.txt"
@@ -63,13 +73,12 @@ sync_flag = 0
 # Start recording loop
 try:
     while piNum is not None:
-        now = datetime.now()
-        if time_in_range(strtm, endtm):
+        time_in_range(end)
+        if recstatus == "rec":
             sysTime = datetime.now()
             startTime = sysTime.strftime("%Y-%m-%d_%H_%M_%S")
             outName = f"/home/pi/LamPi/sync/videos/lampivid_{piNum}_{startTime}.h264"
             motionName = f"/home/pi/LamPi/sync/videos/lampivid_{piNum}_{startTime}.mot"
-
             # print(f"Recording {os.path.basename(outName)}")
             # print("Click 'Stop'or press Ctrl+C to interrupt execution\n")
             camera.start_recording(
@@ -77,43 +86,19 @@ try:
             )
             camera.wait_recording(clipDuration)
             camera.stop_recording()
-<<<<<<< HEAD
             pi_log(logName, outName)
-            sync_flag = 0
-
+        elif recstatus == "sync":
+            os.system(
+                "rclone copy /home/pi/LamPi/sync/ OneDrive:LamPi -v --checkers 1 --multi-thread-streams 1 --transfers 1"
+            )
+            pi_log("Started Sync", "NA")
+            # print(f"Syncing finished.\nGoing to sleep now, will start recording again at {start}")
+            # print("Zzzzzzzzzz"...")
+        elif recstatus == "wait":
+            pi_log("Waiting", "NA")
         else:
-            if sync_flag == 0:
-                os.system(
-                    "rclone copy /home/pi/LamPi/sync/ OneDrive:LamPi/test_flopi1 -q"
-                )
-                pi_log(logName, "Rclone started")
-                sync_flag = 1
-            else:
-                pi_log(logName, "Sleeping")
-                tm.sleep(60)
-
-=======
-            cpuTemp = round(CPUTemperature().temperature, 1)
-            diskUsage = shutil.disk_usage("/")
-            diskFree = round(diskUsage.free / (1000000000), 2)
-            logOut = f"Finished recording\n{outName}\nCPU temp: {cpuTemp}\nFree disk space: {diskFree}Gb\n"
-            logFile = open(logName, "a")
-            logFile.write(logOut)
-            logFile.close()
-            print(logOut)            
-        else:
-            sleepTime = (datetime.combine(datetime.now().date(), strtm) - datetime.now()).total_seconds()
-            #print(sleepTime)
-            logFile = open(logName, "a")
-            logMsg = f"Recording loop ended at {datetime.now()\nStarting rcloud syncing..."
-            logFile.write(logMsg)
-            logFile.close()
-            print(logMsg)
-            os.system("rclone copy /home/pi/LamPi/sync/ OneDrive:LamPi -v")
-            print(f"Syncing finished.\nGoing to sleep now, will start recording again at {strtm}")
-            print("Zzzzzzzzzz"...")
-            tm.sleep(sleepTime)
->>>>>>> 6e0eea450a00d0b7b3187e1e665137ff4326a98c
+            print("something went wrong")
+            break
 
 except KeyboardInterrupt:
     try:
